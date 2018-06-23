124be16bd26ddea0d4ce8f253e6012aa1cdd8922
Geová Ramalho dos Santos <gevsantos@hotmail.com>
2018-06-23 19:19:30 -0300

    Início do Projeto

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php 
-
-require_once("vendor/autoload.php");
-
-$app = new \Slim\Slim();
-
-$app->config('debug', true);
-
-$app->get('/', function() {
-    
-	echo "OK";
-
-});
-
-$app->run();
-
- ?>
+ require_once("vendor/autoload.php");
+ $app = new \Slim\Slim();
+ $app->config('debug', true);
+ $app->get('/', function()
+ {
+  echo "OK";
+ });
+ $app->run();
+?>
